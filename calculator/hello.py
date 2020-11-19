0f6e676829b4c027bbfc7103fe19b0f8540a8691
Limo <liam@acordsoftware.tech>
2020-11-20 07:36:58 +0800

    Install venv & create instance of application
    https://www.notion.so/Python-152af34d20b940a588e2ce8d04bf0973?p=4df182adca1e4f6387fedecb31e9d9c2
    
    https: //github.com/irisdroidology/python-learning/issues/3
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- calculator/hello.py
+++ calculator/hello.py
@@ -1,7 +1,17 @@
-
 import sys
 
 # 1. Import `QApplication` and all the required widgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+
+# 2. Instance of QApplication
+app = QApplication(sys.argv) 
+
+# 3. Create an instance of your application's GUI
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 280, 80)
+window.move(60, 15)
+helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
+helloMsg.move(60, 15)
