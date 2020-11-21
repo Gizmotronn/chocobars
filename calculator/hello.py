0beacd60bc225170c7c07e23c4d984a77d72c221
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-11-21 10:58:30 +0800

    Add notes about prev commit & show app gui, run event/main loop as well
    https://www.notion.so/GUI-Calculator-Ap-4df182adca1e4f6387fedecb31e9d9c2
    https://sk.acord.software/4df182adca1e4f6387fedecb31e9d9c2
    @IrisDroidology @acord-robotics/voicebox (for python)
    https://github.com/irisdroidology/python-learning/issues/3 --> need to add our own issue to this repo

--- calculator/hello.py
+++ calculator/hello.py
@@ -15,3 +15,9 @@ window.setGeometry(100, 100, 280, 80)
 window.move(60, 15)
 helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
 helloMsg.move(60, 15)
+
+# 4. Show your application's GUI
+window.show()
+
+# 5. Run your application's event loop (or main loop)
+sys.exit(app.exec_())
