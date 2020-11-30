8023b9b79e283660d0034855539be0c6bba8c88e
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-11-30 01:13:00 +0000

    #1 add buttons to grid

--- calculator/pycalc.py
+++ calculator/pycalc.py
@@ -46,6 +46,39 @@ class PyCalcUi(QMainWindow):
         self.display.setReadOnly(True)
         # Add the display to the general layout
         self.generalLayout.addWidget(self.display)
+    def _createButtons(self):
+        """Create the buttons."""
+        self.buttons = {} # empty dictionary to hold the calc buttons
+        buttonsLayout = QGridLayout() #create a temporary dictionary to store their labels and relative positions on the grid layout, `buttons`
+        # Button text | position on the QGridLayout
+        buttons = {'7': (0, 0),
+                   '8': (0, 1),
+                   '9': (0, 2),
+                   '/': (0, 3),
+                   'C': (0, 4),
+                   '4': (1, 0),
+                   '5': (1, 1),
+                   '6': (1, 2),
+                   '*': (1, 3),
+                   '(': (1, 4),
+                   '1': (2, 0),
+                   '2': (2, 1),
+                   '3': (2, 2),
+                   '-': (2, 3),
+                   ')': (2, 4),
+                   '0': (3, 0),
+                   '00': (3, 1),
+                   '.': (3, 2),
+                   '+': (3, 3),
+                   '=': (3, 4),
+                  }
+        # Create the buttons and add them to the grid layout
+        for btnText, pos in buttons.items(): # create the buttons and add them to both self.buttons and buttonsLayout.
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(40, 40) # Every button will have a fixed size of 40x40 pixels
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+        # Add buttonsLayout to the general layout
+        self.generalLayout.addLayout(buttonsLayout)
 
 # Client code
 def main():
