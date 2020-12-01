fcd9d77f8a46f68728cfb5af9da9419b599f9429
Blackmagicknight <61495292+Blackmagicknight@users.noreply.github.com>
2020-12-01 16:34:00 +0800

    fix tab errors

--- calculator/pycalc.py
+++ calculator/pycalc.py
@@ -22,11 +22,12 @@ __author__ = 'Leodanis Pozo Ramos'
 # Create a subclass of QMainWindow to setup the calculator's GUI
 class PyCalcUi(QMainWindow):
     """PyCalc's View (GUI)."""
+
     def __init__(self):
         """View initializer."""
         super().__init__()
         # Set some main window's properties
-        self.setWindowTitle('PyCalc')
+        self.setWindowTitle("PyCalc")
         self.setFixedSize(235, 235)
         # Set the central widget and the general layout
         self.generalLayout = QVBoxLayout()
@@ -36,7 +37,8 @@ class PyCalcUi(QMainWindow):
         # Create the display and the buttons
         self._createDisplay()
         self._createButtons()
-	def _createDisplay(self):
+
+    def _createDisplay(self):
         """Create the display."""
         # Create the display widget
         self.display = QLineEdit()
@@ -46,54 +48,56 @@ class PyCalcUi(QMainWindow):
         self.display.setReadOnly(True)
         # Add the display to the general layout
         self.generalLayout.addWidget(self.display)
+
     def _createButtons(self):
         """Create the buttons."""
-        self.buttons = {} # empty dictionary to hold the calc buttons
-        buttonsLayout = QGridLayout() #create a temporary dictionary to store their labels and relative positions on the grid layout, `buttons`
+        self.buttons = {}
+        buttonsLayout = QGridLayout()
         # Button text | position on the QGridLayout
-        buttons = {'7': (0, 0),
-                   '8': (0, 1),
-                   '9': (0, 2),
-                   '/': (0, 3),
-                   'C': (0, 4),
-                   '4': (1, 0),
-                   '5': (1, 1),
-                   '6': (1, 2),
-                   '*': (1, 3),
-                   '(': (1, 4),
-                   '1': (2, 0),
-                   '2': (2, 1),
-                   '3': (2, 2),
-                   '-': (2, 3),
-                   ')': (2, 4),
-                   '0': (3, 0),
-                   '00': (3, 1),
-                   '.': (3, 2),
-                   '+': (3, 3),
-                   '=': (3, 4),
-                  }
+        buttons = {
+            "7": (0, 0),
+            "8": (0, 1),
+            "9": (0, 2),
+            "/": (0, 3),
+            "C": (0, 4),
+            "4": (1, 0),
+            "5": (1, 1),
+            "6": (1, 2),
+            "*": (1, 3),
+            "(": (1, 4),
+            "1": (2, 0),
+            "2": (2, 1),
+            "3": (2, 2),
+            "-": (2, 3),
+            ")": (2, 4),
+            "0": (3, 0),
+            "00": (3, 1),
+            ".": (3, 2),
+            "+": (3, 3),
+            "=": (3, 4),
+        }
         # Create the buttons and add them to the grid layout
-        for btnText, pos in buttons.items(): # create the buttons and add them to both self.buttons and buttonsLayout.
+        for btnText, pos in buttons.items():
             self.buttons[btnText] = QPushButton(btnText)
-            self.buttons[btnText].setFixedSize(40, 40) # Every button will have a fixed size of 40x40 pixels
+            self.buttons[btnText].setFixedSize(40, 40)
             buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
 
-        # Display text methods
-        def setDisplayText(self, text):
-            """Set display's text."""
-            self.display.setText(text) # set and update the text on the display
-            self.display.setFocus() # set the cursor's focus on the display
+    def setDisplayText(self, text):
+        """Set display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
 
-        def displayText(self):
-            """Get display's text."""
-            return self.display.text() # returns the current text - to the display after the = sign is clicked
+    def displayText(self):
+        """Get display's text."""
+        return self.display.text()
 
-        def clearDisplay(self):
-            """Clear the display."""
-            self.setDisplayText('') # sets the display's text to an empty string
-
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText("")
+	
+	
 # Client code
 def main():
     """Main function."""
